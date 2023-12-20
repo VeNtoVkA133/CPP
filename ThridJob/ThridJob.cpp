6409b5829dae8a9094a6a034215d5706849d7274
VeNtoVkA133 <svyatoslav.stolyarov.s.sss2000@mail.ru>
2023-12-20 05:11:58 +0300

    ThridJob
    Третья лаба не полная

---
+++ ThridJob/ThridJob.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	double y;
+
+	for (double x = -3; x <= 3; x += 0.25) {
+		if (x > 2) {
+			y = exp(x);
+		}
+		else if (x >= -2 && x <= 2) {
+			y = x + 4;
+		}
+		else {
+			y = 0;
+		}
+		cout << y << endl;
+	}
+
+	system("pause");
+}
